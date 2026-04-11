76c632078e93d82fd49c61aa830034307c1264ce
Mukhammad <nomonhojaevm@gmail.com>
2026-04-11 18:28:59 +0900

    BRR: Initial setup

---
+++ memo.py
@@ -0,0 +1,8 @@
+"""
+PYTHON USAGE :
+    - Web-Full Stack (Django & FastAPI) ⭐️
+    - AI enginerring (ComVision & NLP) ⭐️
+    - Automation / Testing (Selenium) ⭐️
+    - Desktop / Testing (Kivy $ BeeWare) ⭐️
+    - Hacking(CSRF $ Injection) ⭐️
+"""
