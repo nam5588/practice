ecaf8bd324a3647e3b8b5e7285a629c13aa0a01f
Mukhammad <nomonhojaevm@gmail.com>
2026-04-13 21:16:05 +0900

    feat: learn python booleans

--- primitive.py
+++ primitive.py
@@ -1,6 +1,3 @@
-import re
-
-
 print("======== number ========")
 # in Java, variable is a name of storege location!
 # in Python, variable is a named reference!
@@ -31,4 +28,20 @@ print(f"the result (4): {result3}")
 result3 = course.replace("FullStack", "MasterClass")
 print(f"the result (4): {result3}")
 
+
 print("======== boolean ========")
+# functions > type() input() boo() int() str()
+# y = input("Give your value for y: ")
+# print('y:', y)
+# result = y.isnumeric()
+# print(f"the input value is numeric: {result}")
+
+# TRUTHY vs FALSY value
+# TRUTHY: True, 100, -100, "MIT"
+# FALSY: False, 0, "", None
+
+test_falsy = "" or False or None or 0
+print('The FALSY:', bool(test_falsy))
+
+test_truthy = "MIT"
+print('The TRUTHY:', bool(test_truthy))
