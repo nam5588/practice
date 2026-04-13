65a3a12bb2237e93d2a6482024e65ab2af94a51a
Mukhammad <nomonhojaevm@gmail.com>
2026-04-13 20:03:42 +0900

    feat: python system variables and special methods via dunder

--- run.py
+++ run.py
@@ -1,1 +1,14 @@
-print("Hello world")
+# Dunder __buildins__ ,  __init__
+
+message = "PYTHON: Evething is object!"
+print(message)
+
+result = type(message)
+print("Type of message:", result)
+
+""" In Python, there are buildin tools:
+ (1) TYPES > int float str list dict 
+ (2) FUNCTIONS > print() len() input() type()
+ (3) CONSTANTS > True False None
+"""
+# show all buildins > print(dir(__builtins__))
