6f999c0c2c052cd733f951e4c01ef7b35b35103d
Mukhammad <nomonhojaevm@gmail.com>
2026-04-26 15:36:29 +0900

    feat: learn python magic methods in class

--- class.about.py
+++ class.about.py
@@ -1,9 +1,10 @@
 """Class
     (1) What is class
     (2) Ordinary vs Static properties
-    (3) Special methods
+    (3) Special methods __ @@@__
 """
 
+from encodings.punycode import T
 from hmac import new
 
 
@@ -49,3 +50,49 @@ new_message = Person.message
 
 print("new_message:", new_message)
 Person.explain()
+
+print("======= Special/magic methods =======")
+# Python's most common special methods are below:
+# __init__ __new__ __str__ __call__ __getitem__ __eq__ __len__ ...
+
+
+class Car():
+    # state
+    description = 'This class makes cars'
+
+    # constructor
+    def __new__(cls, *args):
+        print("* __new__ *")
+        return super().__new__(cls)
+
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
+
+    # method
+    def start_enginee(self):
+        print(f"the {self.name}, started engine!")
+
+    def stop_enginee(self):
+        print(f"the {self.name}, stopped engine!")
+
+    def __str__(self):
+        return f"{self.name} was produced in {self.year} year!"
+
+    def __call__(self):
+        print("Object called as function!")
+        return True
+
+
+my_car = Car("SM3", 2015)
+my_car.start_enginee()
+my_car.stop_enginee()
+
+print('-----')
+your_car = Car("KIA", 2013)
+print(your_car)  # __str__ bilan tashkil etdik aks holsa bu >=>=> <__main__.Car object at 0x104efd3d0> <=<=<= bolishi kk edi
+# CALL      # __call__ orqali biz buni callable holatga keltirdik)
+response = your_car()
+print(response)
+
+""""Maxsus magic methodlarni ishlatib turli amallarni bajarib olish mumkin!"""
