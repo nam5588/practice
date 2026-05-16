12e87f361525ea4d37504af84ef696f54f7e382c
Mukhammad <nomonhojaevm@gmail.com>
2026-05-16 16:52:11 +0900

    feat: enumerate, map and filter

--- list.py
+++ list.py
@@ -2,7 +2,7 @@
     (1) Working with list
     (2) List methods
     (3) Lambda function
-    (4) enumarate, map and filter
+    (4) enumerate, map and filter
 """
 import numbers
 from pickle import TRUE
@@ -119,8 +119,61 @@ people.sort()   # sort by ABC -> (aplhabet)
 print("people(1):", people)
 
 # sort by age via lambda
-people.sort(key=lambda person:person[1])
+people.sort(key=lambda person: person[1])
 print("people(2):", people)
+
+print()
+
+
+print("====== Enumerate, map and filter ======")
+# enumerate for index & value
+print()
+animals = ['dog', 'cat']
+for el in enumerate(animals):       # enumerate() - value ning key i bilan chiqaradi
+    print('el:', el)                # el: (0, 'dog')
+
+print()
+for (index, value) in enumerate(animals):
+    print(f'The index: {index} and value: {value}')
+
+print()
+# similar in dictionaries
+car_obj = dict(brand="Ferrari", year=2026)
+# dict ichidagi key va valuelarni     > array ichida  tuple qilib yoyib beradi
+result = car_obj.items()
+print('result:', result)
+for key, value in result:
+    print(f'The key: {key} and value: {value}')
+print('-----------')
+
+
+# map
+cars = [
+    ('Ferrari', 78),
+    ('Toyota', 87),
+    ('Audi', 116),
+    ('BMW', 109),
+    ('Pagani', 33)
+]
+
+new_cars = []
+for car in cars:
+    new_cars.append(car[0])
+print('new_cars(1):', new_cars)
+print()
+
+
+result_map = map(lambda car: car[0], cars)
+print(f'result_map: {result_map} and type: {type(result_map)}')
+new_cars = list(result_map)
+print('new_cars(2):', new_cars)
+print()
+print('--------')
+# filter
+
+result_filter = filter(lambda car: car[1] > 80, cars)
+print(f'result_filter: {result_filter} and type: {type(result_filter)}')
+print(list(result_filter))
 
 
 print()
