183f19a51733654beed317fb645650088be6c51a
Mukhammad <nomonhojaevm@gmail.com>
2026-06-01 00:08:13 +0900

    feat: learn set and set operators

--- array.set.py
+++ array.set.py
@@ -24,3 +24,37 @@ print('numbers(3): ', numbers)
 del numbers[1:3]
 
 print('numbers(4): ', numbers)
+print('\n\n\n')
+
+
+print("===== Set =====")
+# { set } of unique collection without keeping order!
+new_numbers = array('i', [1, 4, 9, 7, 4, 1, 3, 4, 5])
+numbs_set = set(new_numbers)
+print(f'numbs_set: {numbs_set} and type: {type(numbs_set)}')
+
+numbs_set.add(200)
+print('numbs_set(1): ', numbs_set)
+
+numbs_set.add(4)
+print('numbs_set(2): ', numbs_set)
+print('\n\n\n')
+
+
+print("===== Secific oprators with set =====")
+# | & - ^
+
+a = {10, 20, 50}
+b = {20, 40}
+
+result1 = a | b # union
+result2 = a & b # intersection
+result3 = a - b # difference
+result4 = a ^ b # symemetric difference
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+
+print('\n')
