b0dd607b6d6d3b3a7d10db4e3748a6426d694bf6
Mukhammad <nomonhojaevm@gmail.com>
2026-06-09 01:13:13 +0900

    feat: debugging

--- debugging.py
+++ debugging.py
@@ -30,10 +30,9 @@ print('---------------------')
 # with
 with open('material/msg.txt', 'r') as your_file:
     your_content = your_file.read()
-    print("your_content:",your_content)
-    
-print("--- DONE ---")
+    print("your_content:", your_content)
 
+print("--- DONE ---")
 
 
 print('====== Package Manager & External Package ======')
@@ -43,7 +42,25 @@ print('====== Package Manager & External Package ======')
 '''
 # External packages > https://pypi.org/
 
-with Image.open('material/logo.png') as img_obj:
-    resized = img_obj.resize((200, 200))
-    resized.show()
-    resized.save("material/sample.png")
+# with Image.open('material/logo.png') as img_obj:
+#     resized = img_obj.resize((200, 200))
+#     resized.show()
+#     resized.save("material/sample.png")
+print("\n\n\n\n\n\n\n\n\n")
+
+print('====== Debugging ======')
+
+
+def get_summary(*args):     # DEFINE
+    total_amount = 0
+    for a in args:
+        total_amount += a
+    return total_amount
+
+
+# CALL
+res = get_summary(1, 2, 3, 4, 6)
+
+print(res)
+print("\n\n\n\n\n\n\n")
+
